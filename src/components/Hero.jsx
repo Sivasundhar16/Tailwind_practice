a9ac6deae61a143868913c8195c5f13fcec2a0ac
Sivasundhar <vssiva18@gmail.com>
2024-11-09 11:53:14 +0530

    modified project for portfoloi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,19 @@
 export const Hero = () => {
-  return <div className="text-green-400 uppercase">Hero</div>;
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] h-screen mx-auto text-center flex justify-center">
+        <p className="text-[#00df9a] font-bold p-2  ">
+          Growing with Data Analytics
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl font-bold md:py-6">
+          grow with data
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast , Flexible , Financing for{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
